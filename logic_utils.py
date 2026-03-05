05ff32d6ad3b2812e156794be3df3773fc92313c
Jessy-Ding <89423283+Jessy-Ding@users.noreply.github.com>
2026-03-04 22:25:57 -0800

    FIX: Exception handler should convert both to strings

--- logic_utils.py
+++ logic_utils.py
@@ -51,11 +51,12 @@ def check_guess(guess, secret):
             return "Too Low", "📉 Go HIGHER!"
     except TypeError:
         g = str(guess)
-        if g == secret:
+        s = str(secret) 
+        if g == s:
             return "Win", "🎉 Correct!"
-        if g > secret:
-            return "Too High", "📈 Go HIGHER!"
-        return "Too Low", "📉 Go LOWER!"
+        if g > s:
+            return "Too High", "📈 Go LOWER!"
+        return "Too Low", "📉 Go HIGHER!"
 
 
 def update_score(current_score: int, outcome: str, attempt_number: int):
